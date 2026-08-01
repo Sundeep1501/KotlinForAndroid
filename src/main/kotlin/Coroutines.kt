0fcaf34c6493214918ec73c635197e9ef7939312
Sundeep Penkay <sundeep1501@gmail.com>
2026-08-01 10:49:30 -0700

    Coroutines launch{} launches new child coroutines

--- src/main/kotlin/Coroutines.kt
+++ src/main/kotlin/Coroutines.kt
@@ -1,22 +1,34 @@
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
-import kotlin.time.Duration.Companion.seconds
+import kotlin.time.Duration.Companion.milliseconds
 
 suspend fun fetchMessage(
     messageId: MessageId,
+    delayMillis: Long,
 ): String {
-    delay(1.seconds)
+    delay(delayMillis.milliseconds)
     return "Message: ${messageId.value}"
 }
 
 
 fun main() = runBlocking {
-    println("Before fetch")
+    launch {
+        val message = fetchMessage(
+            messageId = MessageId.from("message-1"),
+            delayMillis = 1_000L
+        )
+        println(message)
+    }
 
-    val message = fetchMessage(
-        MessageId.from("message-123"),
-    )
+    launch {
+        val message = fetchMessage(
+            messageId = MessageId.from("message-2"),
+            delayMillis = 500L,
+        )
+        println(message)
+    }
 
-    println(message)
-    println("After fetch")
+
+    println("Requests started")
 }
